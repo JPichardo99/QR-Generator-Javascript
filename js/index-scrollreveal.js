38f23cead82ded08c4fe4e308d5477950adcf15c
jesus pichardo <alepbuenrostro20@gmail.com>
2023-07-24 17:33:17 -0600

    QR-Generator-Javascript

---
+++ js/index-scrollreveal.js
@@ -0,0 +1,10 @@
+ScrollReveal().reveal(".container", {
+	duration: 1800,
+	origin: "bottom",
+	distance: "-100px",
+});
+ScrollReveal().reveal(".info-autor", {
+	duration: 1800,
+	origin: "right",
+	distance: "-100px",
+});
---
+++ js/script.js
@@ -0,0 +1,15 @@
+let imgBox = document.getElementById('imgBox');
+let qrImage = document.getElementById('qrImage');
+let qrText = document.getElementById('qrText');
+
+function generateQR() {
+	if (qrText.value.length > 0) {
+		qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+		imgBox.classList.add("show-img");
+	} else {
+		iziToast.warning({
+			title: 'Error',
+			message: 'Please enter a text or URL to generate QR code.',
+		});
+	}
+}
